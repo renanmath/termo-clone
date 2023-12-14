d9f830f4b2c3055ecaaa9e1cc9e892ae2f2e28dc
renan <renanmath13@gmail.com>
2023-12-14 20:28:35 -0300

    feat: mock all words in constants

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,38 +1,19 @@
-import axios from "axios"
 
-import { WORDS_API_URL, WordData } from "@/constants"
+import { FREQUENT_WORDS } from "@/constants"
 import Game from "@/components/game/game"
 import { GameStateContextProvider } from "@/context/game-state-context"
 
 
-async function Home() {
+async function Home() {  
 
-    try {
+    const allWords: string[] = FREQUENT_WORDS.split("\n")
 
-        const response = await axios.get(WORDS_API_URL)
-        const rawData: string = response.data
-        const allData: string[] = rawData.split("\n")
-        const allWordData: WordData[] = allData.slice(1).map(data => {
-            const dataSplited = data.split(",")
-            return { word: dataSplited[0], frequency: Number(dataSplited[1]) } as WordData
-        })
+    return (
+      <GameStateContextProvider>
+        <Game allWords={allWords} />
+      </GameStateContextProvider>
 
-        return (
-            <GameStateContextProvider>
-                <Game allWordData={allWordData} />
-            </GameStateContextProvider>
-
-        );
-    }
-
-    catch (error) {
-        return (
-            <div>
-                404
-            </div>
-        )
-
-    }
+    );
 }
 
 export default Home;
--- components/game/game.tsx
+++ components/game/game.tsx
@@ -1,62 +1,56 @@
 "use client";
 
-import { WordData } from "@/constants"
 import GameBoard from "./game-board"
-import { useEffect, useMemo } from "react"
+import { useEffect } from "react"
 import { useGameState } from "@/context/game-state-context";
 
 type GameProps = {
-    allWordData: WordData[]
+  allWords: string[]
 }
 
-function Game({ allWordData }: GameProps) {    
-    const wordSize = 5
-    const numWords = 4
-    const frequencyThreshold = 10000
+function Game({ allWords }: GameProps) {
+  const wordSize = 5
+  const numWords = 4
 
-    const allWords = useMemo(() => allWordData.filter(data => data.frequency >= frequencyThreshold).map(data => data.word), [allWordData, frequencyThreshold]);
+  const filteredWords = allWords.filter(w => w.length === wordSize)
+  const listOfWords = Array.from({ length: numWords }, () => {
+    const wordIndex = Math.floor(Math.random() * filteredWords.length)
+    return filteredWords[wordIndex]
+  })
 
+  const { gameState, changeGameState } = useGameState()
+  console.log("Rendering Game")
 
-    const filteredWords = allWords.filter(w => w.length === wordSize)
-    const listOfWords = Array.from({ length: numWords }, () => {
-        const wordIndex = Math.floor(Math.random() * filteredWords.length)
-        return filteredWords[wordIndex]
-    })
+  function validateChar(char: string) {
+    return /^[a-zA-Z]$/.test(char);
+  }
 
-    const {gameState, changeGameState} = useGameState()
-    console.log("Rendering Game")
+  useEffect(() => {
+    const keyDownHandler = (event: KeyboardEvent) => {
+      console.log('Current stage', gameState);
+      if (validateChar(event.key)) {
+        const newState = { ...gameState }
+        newState.currentChar = event.key
+        console.log("New State", newState)
+        changeGameState(newState)
+      }
 
-    function validateChar(char:string) {
-      return /^[a-zA-Z]$/.test(char);
-    }
+    };
 
-    useEffect(() => {
-        const keyDownHandler = (event: KeyboardEvent) => {
-          console.log('Current stage', gameState);
-          if (validateChar(event.key)){
-            const newState = {...gameState}
-            newState.currentChar = event.key
-            console.log("New State", newState)
-            changeGameState(newState)
-          }
-    
-        };
-    
-        document.addEventListener('keydown', keyDownHandler);
-    
-        return () => {
-          document.removeEventListener('keydown', keyDownHandler);
-        };
-      }, [gameState]);
-    
+    document.addEventListener('keydown', keyDownHandler);
 
-    return (
-        <div>
-            <GameBoard
-                listOfWords={listOfWords}
-                numRows={wordSize + numWords} />
-        </div>
-    );
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, [gameState]);
+
+  return (
+    <div>
+      <GameBoard
+        listOfWords={listOfWords}
+        numRows={wordSize + numWords} />
+    </div>
+  );
 }
 
 export default Game;
