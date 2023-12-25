bcd96c4ba424babf67605526639842d95ecbae12
renan <renanmath13@gmail.com>
2023-12-25 11:08:22 -0300

    feat: update word when type using keyboard

--- components/keyboard/key-box.tsx
+++ components/keyboard/key-box.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { validateChar } from "@/lib/game-utils";
+import { useGame } from "@/context/game-context";
+
 export type KeyProps = {
     char: string,
 
 }
 
 function KeyBox({char}: KeyProps) {
+
+    const {gameState, updateWord, configurations} = useGame()
+
+    function handleOnClick(){
+        if (validateChar(char)){
+            console.log(gameState)
+            updateWord(gameState, char.toLowerCase(), configurations.wordSize)
+        }
+    }
+
     return (
     <div className="bg-slate-800 p-1 rounded-sm text-lg text-white font-bold w-[32px] h-[32px] text-center hover:bg-slate-900">
-        <button>
+        <button
+        onClick={handleOnClick}>
             {char.toUpperCase()}
         </button>
     </div> );
